f266075dc0d28e709ea329cf70ef0d734231d322
sanctusgee <godwin.dsg@gmail.com>
2015-10-18 00:41:28 -0600

    update c8773-20151018

--- config/EXAMPLE_api_keys.py
+++ config/EXAMPLE_api_keys.py
@@ -1,6 +1,14 @@
+####
+##
+#
 # API keys go here
-#   xxxxxxxxx
+#
+#####################
 
+## the API_KEYS listed below are samples
+## Refer to
+    # http://openweathermap.org/appid#get to get required API_KEY for OpenWeather
+    # http://www.wunderground.com/weather/api/ for WeatherUnderground API_KEY
 api_keys = {
     'OPEN_WEATHER_MAP': 'dc84892b4bb2449gbc0ce8734b42ea74e8643d8',
     'WEATHER_UNDERGROUND': 'xxxy09876521yzzz'
