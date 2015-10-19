cb85a7ce83f034d3a8b8fc253e463aa5f9602901
sanctusgee <godwin.dsg@gmail.com>
2015-10-19 11:32:56 -0600

    dynamic widget

--- config/EXAMPLE_api_keys.py
+++ config/EXAMPLE_api_keys.py
@@ -5,11 +5,19 @@
 #
 #####################
 
-## the API_KEYS listed below are samples
-## Refer to
+'''
+    In a Production environment, your API keys should NOT be
+        - made public
+        - pushed to a PUBLIC github (or any public VCS) repository
+
+
+The API_KEYS listed below are samples and will not work
+Refer to
     # http://openweathermap.org/appid#get to get required API_KEY for OpenWeather
     # http://www.wunderground.com/weather/api/ for WeatherUnderground API_KEY
+'''
+
 api_keys = {
-    'OPEN_WEATHER_MAP': 'dc84892b4bb2449gbc0ce8734b42ea74e8643d8',
-    'WEATHER_UNDERGROUND': 'xxxy09876521yzzz'
+    'OPEN_WEATHER_MAP': 'dc8489fgb2edr2b4bb2449gbc0ce8734b42ea74e8643d8',
+    'WEATHER_UNDERGROUND': '54906dfe789dey09876521yzzz'
 }
