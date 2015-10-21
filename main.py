fb0886fb63bb29d0873e86b28c656ef7887a1583
sanctusgee <godwin.dsg@gmail.com>
2015-10-21 17:52:15 -0600

    special cases

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from kivy.factory import Factory
+# from kivy.factory import Factory
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, ListProperty, StringProperty, NumericProperty
 from kivy.network.urlrequest import UrlRequest
 import json
 from config.api_keys import api_keys
@@ -12,14 +12,14 @@ class WeatherRoot(BoxLayout):
     current_weather = ObjectProperty()
 
     def show_current_weather(self, location=None):
-
         self.clear_widgets()
 
         if location is None and self.current_weather is None:
-            location = "New York (US)"
+            # location = ("New York",  "US")
+            self.current_weather = CurrentWeather(location=location)
 
         if location is not None:
-            self.current_weather = Factory.CurrentWeather()
+            # self.current_weather = Factory.CurrentWeather()
             self.current_weather.location = location
             self.add_widget(self.current_weather)
 
@@ -30,7 +30,7 @@ class WeatherRoot(BoxLayout):
         # self.add_widget(AddLocationForm())
 
 class LocationButton(ListItemButton):
-    pass
+    location = ListProperty()
 
 
 class AddLocationForm(BoxLayout):
@@ -61,7 +61,8 @@ class AddLocationForm(BoxLayout):
 
         # OpenWeatherMap (http://openweathermap.org/) API implementation
         data = json.loads(data.decode()) if not isinstance(data, dict) else data
-        cities = ["{} ({})". format(d['name'], d['sys']['country']) for d in data['list']]
+        # cities = ["{} ({})". format(d['name'], d['sys']['country']) for d in data['list']]
+        cities = [(d['name'], d['sys']['country']) for d in data['list']]
         self.search_results.item_strings = cities
 
         # Python 3 syntax
@@ -71,6 +72,32 @@ class AddLocationForm(BoxLayout):
         self.search_results.adapter.data.extend(cities)
         self.search_results._trigger_reset_populate()
 
+    def args_converter(self, index, data_item):
+        city, country = data_item
+        return{'location': (city, country)}
+
+
+class CurrentWeather(BoxLayout):
+    open_weather_id = api_keys['OPEN_WEATHER_MAP']
+    location = ListProperty(['New York', 'US'])
+    conditions = StringProperty()
+    temp = NumericProperty()
+    temp_min = NumericProperty()
+    temp_max = NumericProperty()
+
+    def update_weather(self):
+        weather_template = "http://api.openweathermap.org/data/2.5/" \
+                           "weather?q={}, {}&units=metric&appid={}"
+        ## put back *self.location
+        weather_url = weather_template.format(self.location, self.open_weather_id)
+        request = UrlRequest( weather_url, self.weather_retrieved)
+
+    def weather_retrieved( self, request, data):
+        data = json.loads(data.decode()) if not isinstance(data, dict) else data
+        self.conditions = data[' weather'][ 0][' description']
+        self.temp = data[' main'][' temp']
+        self.temp_min = data[' main'][' temp_min']
+        self.temp_max = data[' main'][' temp_max']
 
 class WeatherApp(App):
     pass
